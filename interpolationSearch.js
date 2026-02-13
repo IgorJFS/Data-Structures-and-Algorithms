b9a249a9f16c963ce948270643c59e2596819aca
IgorJFS <igormpaz@gmail.com>
2026-02-13 01:32:36 -0300

    .

--- interpolationSearch.js
+++ interpolationSearch.js
@@ -4,42 +4,39 @@
 // Exemplos Reais: Procurar nome em lista telefônica, busca em índices de dados numéricos uniformes.
 
 // Usamos esse algoritmo para dados que crescen de maneira previsivel, como 1,2,3,4,5,6 ou exponencialmente como desse exemplo. Nesses casos, ele até supera o Binary Search
-const array = [1,2,4,8,16,32,64,128,256,512,1024]
+const array = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
 
 let tentativas = 0;
 
-function interpolationSearch(arr, valor){
- let alto = arr.length - 1;
- let baixo = 0;
+function interpolationSearch(arr, valor) {
+  let alto = arr.length - 1;
+  let baixo = 0;
 
- while (valor >= arr[baixo] && valor <= arr[alto] && baixo <= alto) {
-      let probe = Math.floor(
+  while (valor >= arr[baixo] && valor <= arr[alto] && baixo <= alto) {
+    let probe = Math.floor(
       baixo +
-      ((alto - baixo) * (valor - arr[baixo])) /
-      (arr[alto] - arr[baixo])
+        ((alto - baixo) * (valor - arr[baixo])) / (arr[alto] - arr[baixo]),
     );
-  tentativas++
+    tentativas++;
 
-  if (arr[probe] === valor) {
-    return probe;
+    if (arr[probe] === valor) {
+      return probe;
+    } else if (arr[probe] < valor) {
+      baixo = probe + 1;
+    } else {
+      alto = probe - 1;
+    }
   }
-  else if (arr[probe] < valor) {
-    baixo = probe + 1;
-  }
-  else {
-    alto = probe - 1;
-  }
- }
- return -1;
+  return -1;
 }
 
-let index = interpolationSearch(array, 256)
-let palavra = tentativas === 1 ? "tentativa" : "tentativas"
+let index = interpolationSearch(array, 256);
+let palavra = tentativas === 1 ? "tentativa" : "tentativas";
 
 if (index !== -1) {
-  console.log(`Numero encontrado no index: ${index} com ${tentativas} ${palavra}`);
-}
-else{
+  console.log(
+    `Numero encontrado no index: ${index} com ${tentativas} ${palavra}`,
+  );
+} else {
   console.log("Numero não encontrado");
-  
 }
